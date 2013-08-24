e1c51e352bb95e5cec39f34f421137e70870779e
dbrio2011 <dbrio2011@gmail.com>
2013-08-24 19:41:20 +0000

    se agrego una clase para la conexion y se hizo prueba selec en formularios/Historial.vb

---
+++ helpstudent/Conexion.vb
@@ -0,0 +1,10 @@
+﻿Imports System.Data.SQLite
+
+Imports System.Data.SqlClient
+
+Module conexion
+    Public path1 As String = AppDomain.CurrentDomain.BaseDirectory
+    Public dbpath As String = path1.Replace("\bin\Debug\", "\StudentApp.s3db")
+    Public cnnString As String = String.Format("Data Source={0}", dbpath)
+End Module
+
