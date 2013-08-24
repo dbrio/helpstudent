f8354c1c72c3a0126beeee9f9101e2174308ecfb
dbrio2011 <dbrio2011@gmail.com>
2013-08-24 20:31:40 +0000

    se modifico la clase conexion se  agregaron las conexiones temporal y mysql

--- helpstudent/Conexion.vb
+++ helpstudent/Conexion.vb
@@ -1,10 +1,19 @@
 ﻿Imports System.Data.SQLite
-
-Imports System.Data.SqlClient
+Imports MySql.Data.MySqlClient
 
 Module conexion
     Public path1 As String = AppDomain.CurrentDomain.BaseDirectory
     Public dbpath As String = path1.Replace("\bin\Debug\", "\StudentApp.s3db")
     Public cnnString As String = String.Format("Data Source={0}", dbpath)
+
+
+    Public path2 As String = AppDomain.CurrentDomain.BaseDirectory
+    Public dbpath2 As String = path1.Replace("\bin\Debug\", "\StudentApp.s3db")
+    Public cnnString2 As String = String.Format("Data Source={0}", dbpath2)
+
+    Public mysqlconexion As New MySqlConnection(My.Settings.mysql)
+
+
+
 End Module
 
