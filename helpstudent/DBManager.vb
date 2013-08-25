48527f3abb1f29244dbee1eb06d1f6ecbd1175d1
dbrio2011 <dbrio2011@gmail.com>
2013-08-25 17:29:35 +0000

    se creo la conexion en una lase DBManager.vb

---
+++ helpstudent/DBManager.vb
@@ -0,0 +1,57 @@
+﻿Imports System.Data.SQLite
+
+Public Class DBManager
+    Private CNN As New SQLiteConnection
+
+    Public Sub New()
+        Dim path1 As String = AppDomain.CurrentDomain.BaseDirectory
+        Dim dbpath As String = path1.Replace("\bin\Debug\", "\StudentApp.s3db")
+        CNN.ConnectionString = String.Format("Data Source={0}", dbpath)
+    End Sub
+
+    Public Function GetData(ByVal cmd As String, Optional ByVal table As String = "Tablex")
+
+        Try
+            Using DS As New DataSet
+                Using DA As New SQLiteDataAdapter(cmd, CNN)
+                    DA.Fill(DS, table)
+
+                End Using
+                Return DS.Tables(table)
+            End Using
+        Catch ex As Exception
+            Return Nothing
+        End Try
+
+
+    End Function
+
+
+    Public Function NotQuery(ByVal comando As String)
+        Using CMD As New SQLiteCommand(comando)
+            Try
+                CNN.Open()
+                CMD.ExecuteNonQuery()
+                CNN.Close()
+                Return True
+            Catch ex As Exception
+
+            End Try
+        End Using
+    End Function
+
+    Public Function GetLastID(ByVal col As String, ByVal table As String) As Integer
+        Try
+            Dim sql As String = String.Format("SELECT MAX({0}) FROM {1}", col, table)
+            Dim ID As Integer
+
+            Using CMD As New SQLiteCommand(sql, CNN)
+                CNN.Open()
+                ID = CMD.ExecuteScalar
+                CNN.Close()
+            End Using
+        Catch ex As Exception
+            Return Nothing
+        End Try
+    End Function
+End Class
--- helpstudent/formularios/Historial.vb
+++ helpstudent/formularios/Historial.vb
@@ -3,27 +3,19 @@ Imports System.Data.SQLite
 
 
 Public Class Historial
+
     Dim DS As New DataSet
+
 
     Private Sub Historial_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
 
-        If NetworkInterface.GetIsNetworkAvailable Then
-            MsgBox("Tenemos internet ")
-        Else
-            MsgBox("No hay internet ")
-
-        End If
-
-        Using CNN As New SQLiteConnection(cnnString)
-            CNN.Open()
-
-            Using DA As New SQLiteDataAdapter
-                DA.SelectCommand = New SQLiteCommand("SELECT * FROM Clase", CNN)
-                DA.Fill(DS, "Clase")
-                GridControl1.DataSource = DS.Tables("Clase")
-            End Using
-
-            CNN.Close()
-        End Using
+        CargarGrid()
     End Sub
+
+
+    Sub CargarGrid()
+
+        GridControl1.DataSource = db.GetData("SELECT * FROM Clase")
+    End Sub
+
 End Class
