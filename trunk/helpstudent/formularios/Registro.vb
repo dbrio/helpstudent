3739e885c188182bd194d35da94e0f044e4f1b2a
dbrio2011@gmail.com <dbrio2011@gmail.com@757448a7-7c1d-8dbb-5520-35d2298bcec8>
2013-08-25 15:10:52 +0000

    corrigio la db para el inser

--- trunk/helpstudent/formularios/Registro.vb
+++ trunk/helpstudent/formularios/Registro.vb
@@ -9,23 +9,25 @@ Public Class Registro
         If NetworkInterface.GetIsNetworkAvailable Then
             Using CNN As New SQLiteConnection(cnnString)
                 CNN.Open()
-                Dim U As String = "INSERT INTO Login ( Usuario, Contrasena) VALUES ( @Usuario, @Contrasena)"
-                Dim Q As String = "INSERT INTO Alumno (CtaAlum, Nombre, Apellido, FechNac, Telefono, IdSexo,IdLogin, IdCarrera, Correo) VALUES(@CtaAlum, @Nombre, @Apellido, @FechNac, @Telefono, @IdSexo,1,  @IdCarrera, @Correo)"
+
+                Dim Q As String = "INSERT INTO Alumno (CtaAlum, Nombre, Apellido, FechNac, Telefono, IdSexo, IdCarrera, Correo) VALUES(@CtaAlum, @Nombre, @Apellido, @FechNac, @Telefono, @IdSexo, @IdCarrera, @Correo)"
+                Dim U As String = "INSERT INTO Login ( Usuario, Contrasena, CtaAlum) VALUES ( @Usuario, @Contrasena,@CtaAlum )"
 
                 Dim cmd As New SQLiteCommand(Q, CNN)
                 Dim cm As New SQLiteCommand(U, CNN)
                 cmd.Parameters.Add("@CtaAlum", SqlDbType.VarChar, 10).Value = CtaAlumTextEdit.EditValue
                 cmd.Parameters.Add("@Nombre", SqlDbType.VarChar, 30).Value = NombreTextEdit.EditValue
                 cmd.Parameters.Add("@Apellido", SqlDbType.VarChar, 30).Value = ApellidoTextEdit.EditValue
-                cmd.Parameters.Add("@FechNac", SqlDbType.VarChar).Value = DateEdit1.EditValue
+                cmd.Parameters.Add("@FechNac", SqlDbType.Date).Value = DateEdit1.EditValue
                 cmd.Parameters.Add("@Telefono", SqlDbType.VarChar, 9).Value = TelefonoTextEdit.EditValue
                 cmd.Parameters.Add("@IdSexo", SqlDbType.Int).Value = IdSexoTextBox.Text
                 cmd.Parameters.Add("@IdCarrera", SqlDbType.Int).Value = IdCarreraTextBox.Text
                 cmd.Parameters.Add("@Correo", SqlDbType.VarChar, 50).Value = CorreoTextEdit.EditValue
                 cm.Parameters.Add("@Usuario", SqlDbType.VarChar, 10).Value = UsuarioTextBox.Text
                 cm.Parameters.Add("@Contrasena", SqlDbType.VarChar, 40).Value = ContrasenaTextBox.Text
-                cm.ExecuteNonQuery()
                 cmd.ExecuteNonQuery()
+                cm.ExecuteNonQuery()
+
 
                 MsgBox("El Gallo ya canto")
                 CNN.Close()
