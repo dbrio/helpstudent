9f52d00e58fe2248dabcbb793c53b6504c2447a5
jmevolution2@gmail.com <jmevolution2@gmail.com@757448a7-7c1d-8dbb-5520-35d2298bcec8>
2013-09-14 18:49:49 +0000

    ActualizarNotas con validacion

---
+++ trunk/helpstudent/formularios/ActualizarNotas.Designer.vb
@@ -0,0 +1,188 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ActualizarNotas
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBoxClase = New System.Windows.Forms.TextBox()
+        Me.TextBoxHora = New System.Windows.Forms.TextBox()
+        Me.TextBoxDía = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.TextBoxNota3 = New System.Windows.Forms.TextBox()
+        Me.TextBoxNota2 = New System.Windows.Forms.TextBox()
+        Me.TextBoxNota1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(28, 28)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(57, 24)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Clase"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(390, 28)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(51, 24)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Hora"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(219, 28)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(37, 24)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Día"
+        '
+        'TextBoxClase
+        '
+        Me.TextBoxClase.Location = New System.Drawing.Point(13, 57)
+        Me.TextBoxClase.Name = "TextBoxClase"
+        Me.TextBoxClase.Size = New System.Drawing.Size(100, 20)
+        Me.TextBoxClase.TabIndex = 3
+        '
+        'TextBoxHora
+        '
+        Me.TextBoxHora.Location = New System.Drawing.Point(364, 57)
+        Me.TextBoxHora.Name = "TextBoxHora"
+        Me.TextBoxHora.Size = New System.Drawing.Size(100, 20)
+        Me.TextBoxHora.TabIndex = 4
+        '
+        'TextBoxDía
+        '
+        Me.TextBoxDía.Location = New System.Drawing.Point(180, 57)
+        Me.TextBoxDía.Name = "TextBoxDía"
+        Me.TextBoxDía.Size = New System.Drawing.Size(100, 20)
+        Me.TextBoxDía.TabIndex = 5
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(10, 150)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(59, 24)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Nota1"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(12, 231)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(59, 24)
+        Me.Label5.TabIndex = 7
+        Me.Label5.Text = "Nota3"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(10, 191)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(59, 24)
+        Me.Label6.TabIndex = 8
+        Me.Label6.Text = "Nota2"
+        '
+        'TextBoxNota3
+        '
+        Me.TextBoxNota3.Location = New System.Drawing.Point(86, 231)
+        Me.TextBoxNota3.Name = "TextBoxNota3"
+        Me.TextBoxNota3.Size = New System.Drawing.Size(100, 20)
+        Me.TextBoxNota3.TabIndex = 9
+        '
+        'TextBoxNota2
+        '
+        Me.TextBoxNota2.Location = New System.Drawing.Point(86, 191)
+        Me.TextBoxNota2.Name = "TextBoxNota2"
+        Me.TextBoxNota2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBoxNota2.TabIndex = 10
+        '
+        'TextBoxNota1
+        '
+        Me.TextBoxNota1.Location = New System.Drawing.Point(86, 150)
+        Me.TextBoxNota1.Name = "TextBoxNota1"
+        Me.TextBoxNota1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBoxNota1.TabIndex = 11
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(327, 269)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(181, 46)
+        Me.Button1.TabIndex = 12
+        Me.Button1.Text = "Actualizar Notas"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ActualizarNotas
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(529, 327)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBoxNota1)
+        Me.Controls.Add(Me.TextBoxNota2)
+        Me.Controls.Add(Me.TextBoxNota3)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.TextBoxDía)
+        Me.Controls.Add(Me.TextBoxHora)
+        Me.Controls.Add(Me.TextBoxClase)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "ActualizarNotas"
+        Me.Text = "ActualizarNotas"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents TextBoxClase As System.Windows.Forms.TextBox
+    Friend WithEvents TextBoxHora As System.Windows.Forms.TextBox
+    Friend WithEvents TextBoxDía As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents TextBoxNota3 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBoxNota2 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBoxNota1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+End Class
---
+++ trunk/helpstudent/formularios/ActualizarNotas.vb
@@ -0,0 +1,40 @@
+﻿
+Public Class ActualizarNotas
+    Dim NotaID As Integer
+
+    Public Property NOtaID1 As Integer
+        Get
+            Return NotaID
+        End Get
+        Set(ByVal value As Integer)
+            NotaID = value
+        End Set
+    End Property
+
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+        If Not IsNumeric(TextBoxNota1.Text) Then
+            MsgBox("Ingrese cantidad Numerica")
+            TextBoxNota1.Clear()
+            TextBoxNota1.Focus()
+        ElseIf Not IsNumeric(TextBoxNota2.Text) Then
+            MsgBox("Ingrese cantidad Numerica")
+            TextBoxNota2.Clear()
+            TextBoxNota2.Focus()
+        ElseIf Not IsNumeric(TextBoxNota3.Text) Then
+            MsgBox("Ingrese cantidad Numerica")
+            TextBoxNota3.Clear()
+            TextBoxNota3.Focus()
+        Else
+            Dim Promedio = (CInt(TextBoxNota1.Text) + CInt(TextBoxNota2.Text) + CInt(TextBoxNota3.Text)) / 3
+            Dim actualizar As String = String.Format("UPDATE Matricula SET Nota1= '{0}', Nota2 = '{1}', Nota3= '{2}', Promedio = '{3}'    WHERE IdMatricula = {4} ", TextBoxNota1.Text, TextBoxNota2.Text, TextBoxNota3.Text, Promedio, NOtaID1)
+            db.NotQuery(actualizar)
+            MsgBox("Nota Actualizada")
+            Me.Close()
+        End If
+
+            
+
+    End Sub
+End Class
